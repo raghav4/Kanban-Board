910e59117118440ac9f2b4e065382d17a89019a6
Raghav Sharma <raghavsyt@gmail.com>
2020-05-21 11:26:35 +0530

    added apis

--- startup/routes.js
+++ startup/routes.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser');
 const board = require('../routes');
 const welcome = require('../routes/welcome');
 const error = require('../middlewares/error');
 
 module.exports = (app) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/board', board);
   app.use('/', welcome);
   app.use(error);
